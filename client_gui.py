05a554a92bb95220e205393e1ceac67ce46a5954
Bondoc Daniel <daniel.bondoc.it@gmail.com>
2025-05-02 13:38:18 +0300

    Update client_gui.py
    - added transposition method of encryption

--- client_gui.py
+++ client_gui.py
@@ -1,3 +1,4 @@
+import random
 import socket
 import tkinter as tk
 from tkinter import scrolledtext
@@ -15,13 +16,28 @@ def caesar_encrypt(text, key):
             encrypted += char
     return encrypted
 
+
+def transpose_encrypt(text, key):
+    indices = list(range(len(text)))
+    random.seed(key)  
+    random.shuffle(indices)
+    encrypted = ''.join([text[i] for i in indices])
+    return encrypted, indices
+
 class ClientApp:
     def __init__(self, master):
         self.master = master
         master.title("Client")
 
+        self.method = tk.StringVar()
+        self.method.set("caesar")
+
+        tk.Label(master, text="Mesaj:").pack()
         self.entry = tk.Entry(master, width=50)
         self.entry.pack()
+
+        tk.Label(master, text="Metoda de criptare:").pack()
+        tk.OptionMenu(master, self.method, "caesar", "transposition").pack()
 
         self.send_button = tk.Button(master, text="Trimite", command=self.send_message)
         self.send_button.pack()
@@ -32,10 +48,11 @@ class ClientApp:
         self.log = scrolledtext.ScrolledText(master, width=60, height=20, state='disabled')
         self.log.pack()
 
-        self.shared_key = None
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.sock.connect(('localhost', 12345))
-        self.init_key_exchange()
+        self.shared_key = self.key_exchange()
+        self.send_method_to_server()
+        self.log_msg(f"Cheia comună (Diffie-Hellman): {self.shared_key}")
 
     def log_msg(self, msg):
         self.log.config(state='normal')
@@ -43,21 +60,29 @@ class ClientApp:
         self.log.config(state='disabled')
         self.log.see(tk.END)
 
-    def init_key_exchange(self):
-        client_private = 15
-        client_public = pow(g, client_private, p)
-        self.sock.send(str(client_public).encode())
+    def key_exchange(self):
+        private = 15
+        public = pow(g, private, p)
+        self.sock.send(str(public).encode())
         server_public = int(self.sock.recv(1024).decode())
-        self.shared_key = pow(server_public, client_private, p)
-        self.log_msg(f"Cheia comună: {self.shared_key}")
+        return pow(server_public, private, p)
+
+    def send_method_to_server(self):
+        self.sock.send(self.method.get().encode())
 
     def send_message(self):
-        message = self.entry.get()
-        if not message.strip():
+        text = self.entry.get()
+        if not text.strip():
             return
-        encrypted = caesar_encrypt(message, self.shared_key)
+
+        method = self.method.get()
+        if method == "caesar":
+            encrypted = caesar_encrypt(text, self.shared_key)
+        else:
+            encrypted, _ = transpose_encrypt(text, self.shared_key)
+
         self.sock.send(encrypted.encode())
-        self.log_msg(f"Trimis (criptat): {encrypted}")
+        self.log_msg(f"[Trimis cu {method}] {encrypted}")
         self.entry.delete(0, tk.END)
 
     def disconnect(self):
